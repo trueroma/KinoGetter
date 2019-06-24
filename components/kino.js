fa2c85e2be2353482cf53289d2cf35e1f423cc86
Roman Tiutiunnik <trueroma420@gmail.com>
2019-06-24 15:06:56 +0300

    Changed from Https request to fetch, added pictures, and changed desktop design

--- components/kino.js
+++ components/kino.js
@@ -7,16 +7,23 @@ const template =
         margin: 0;
         box-sizing: border-box;
         font-size: 1vw;
+        transition: all .3s;
     }
     .wrapper {
-        width: 23vw;
-        max-width: 400px;
-        height: 40vw;
-        padding: 0 1rem 0;
+        width: 80vw;
+        // max-width: 400px;
+        height: 20vw;
+        // padding: 0 1rem 0;
         border-width: .5em;
         border-style: solid;
         border-image: linear-gradient(45deg, #ff0, #f0f);
         border-image-slice: 1;
+        // overflow-y: auto;
+        display: flex;
+        margin: 0 0 1vw 0;
+    }
+    .content {
+        margin: 1rem;
         overflow-y: auto;
     }
     .name {
@@ -25,36 +32,70 @@ const template =
     .release {
         font-weight: 700;
     }
+    .img {
+        height: 20vw;
+    }
+
+    img {
+        height: 100%;
+    }
 
     @media (max-width: 1500px) {
     :host {
         font-size: 1.6vw;
     }
-    .wrapper {
-        max-width: 550px;
-        width: 37vw;
-        height: 68vw;
-    }
+    // .wrapper {
+    //     max-width: 550px;
+    //     width: 37vw;
+    //     height: 68vw;
+    // }
     @media (max-width: 1000px) {
     :host {
         font-size: 3vw;
     }
     .wrapper {
+        display: block;
         max-width: 740px;
         width: 74vw;
         height: 135vw;
+        overflow-y: auto;
+    }
+    .img {
+        // position: relative;
+        // top: 115vw;
+        width: 100%;
+        height: auto;
+    }
+    img {
+        height: auto;
+        width: 100%;
+    }
+    .content {
+        // position: relative;
+        // top: -20vw;
     }
 }
 </style>
     <div class="wrapper">
-        <h2 class="name"></h2>
-        <p class="overview"></p>
-        <p class="release"></p>
+        <div class="img"></div>
+        <div class="content">
+            <h2 class="name"></h2>
+            <p class="overview"></p>
+            <p class="release"></p>
+        </div>
     </div>
 `;
 
 export default class Kino extends HTMLElement
 {
+    constructor() {
+        super();
+
+        this.name = "Вы шота не ввели";
+        this.overview = "Тут могло быть описание, но вы шота не ввели";
+        this.release = "Тут могла быть дата, но вы шота не ввели";
+    }
+
     connectedCallback()
     {
         this.shadow_root = this.attachShadow({ mode: 'open' });
@@ -63,18 +104,23 @@ export default class Kino extends HTMLElement
         const
             name = this.shadow_root.querySelector('.name'),
             overview = this.shadow_root.querySelector('.overview'),
-            release = this.shadow_root.querySelector('.release');
+            release = this.shadow_root.querySelector('.release'),
+            img = this.shadow_root.querySelector('.img');
+
+            if (this.img.includes('null')) this.img = '../content/KiT.jpg';
 
         name.innerHTML = this.name;
         overview.innerHTML = this.overview;
         release.innerHTML = this.release;
+        img.innerHTML = `<img src="${this.img}" alt="" />`;
         console.log(this.name, this.overview, this.release);
     }
 
-    setDesc(name, overview, release)
+    setDesc(name, overview, release, img)
     {
         this.name = name;
         this.overview = overview;
         this.release = release;
+        this.img = img;
     }
 }
--- main.js
+++ main.js
@@ -10,44 +10,48 @@ const init = () => {
         movie = document.querySelector('.movies');
 
     // functions
-    const apiSearch = () => {
-            const
-                searchText = searchform.value,
-                server = `https://api.themoviedb.org/3/search/multi?api_key=6e32fa26fb5f401e54d8a84c649d1e8f&language=ru&query=${searchText}`;
-            requestApi(server);
-        };
-
-    const requestApi = url => {
-
-        const request = new XMLHttpRequest;
-        request.open('GET', url);
-        request.send();
-
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState != 4) return;
-            if (request.status != 200) {
-                console.log(`error: ${request.status}`);
-                return;
-            }
-
-            while (movie.firstChild) {
-                movie.removeChild(movie.firstChild);
-            }
-
-            const output = JSON.parse(request.responseText);
-
-            let content = '';
-
-            output.results.forEach( item => {
-                const kino = document.createElement('view-kino');
-                kino.setDesc(item.name || item.title, item.overview, item.first_air_date || item.release_date);
-                movie.appendChild(kino);
-            });
+    const show = str => {
+        const urlPoster = `https://image.tmdb.org/t/p/w500/`;
+        while (movie.firstChild) {
+            movie.removeChild(movie.firstChild);
+        }
+        console.log(str.results);
+        str.results.forEach( item => {
+            const kino = document.createElement('view-kino');
+            kino.setDesc(
+                item.name || item.title,
+                item.overview,
+                item.first_air_date || item.release_date,
+                urlPoster + item.poster_path,
+                );
+            movie.appendChild(kino);
         });
-    }
+    };
+    const apiSearch = searchText => {
+        const url = `https://api.themoviedb.org/3/search/multi?api_key=6e32fa26fb5f401e54d8a84c649d1e8f&language=ru&query=${searchText}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(str => show(str))
+            .catch(reason => {
+                console.log(reason);
+            });
+    };
 
     searchform.addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') apiSearch();
+        if (e.key === 'Enter') {   
+            let searchText = searchform.value;
+            searchText = searchText.trim();
+            if (!searchText) {
+                while (movie.firstChild) {
+                    movie.removeChild(movie.firstChild);
+                }
+                const kino = document.createElement('view-kino');
+                movie.appendChild(kino);
+            } else {
+                apiSearch(searchText);
+            }
+        }
     });
 }
 document.addEventListener('DOMContentLoaded', init);
