f564ba4204c0cfb0c3443fcec9550df95e8e1a79
Roman Tiutiunnik <trueroma420@gmail.com>
2019-07-02 18:44:17 +0300

    added personal page for each movie and tvseries, added favicon and made main title as a 'home' button

--- components/kino.js
+++ components/kino.js
@@ -1,3 +1,5 @@
+// import {clear, movies} from '../main.js';
+
 const template =
 `
 <style>
@@ -5,26 +7,38 @@ const template =
     :host {
         padding: 0;
         margin: 0;
-        box-sizing: border-box;
         font-size: 1vw;
-        transition: all .3s;
+    }
+    a {
+        color: #fff;
+        text-decoration: none;
+        background-color: #00f;
+        padding: 1rem;
+        border-radius: 1.3rem;
     }
     .wrapper {
         width: 80vw;
-        // max-width: 400px;
-        height: 20vw;
-        // padding: 0 1rem 0;
+        // height: 30vw;
+        box-sizing: border-box;
         border-width: .5em;
         border-style: solid;
         border-image: linear-gradient(45deg, #ff0, #f0f);
         border-image-slice: 1;
-        // overflow-y: auto;
         display: flex;
         margin: 0 0 1vw 0;
+        overflow: hidden;
+        cursor: pointer;
+    }
+    .wrapper:hover {
+        border-image: linear-gradient(45deg, #f00, #00f);
+        border-image-slice: 1;
+        border-style: solid;
     }
     .content {
-        margin: 1rem;
-        overflow-y: auto;
+        box-sizing: border-box;
+        padding: 1rem 2rem 0;
+        height: 30vw;
+        overflow-y: scroll;
     }
     .name {
         margin: .2rem 0;
@@ -32,12 +46,16 @@ const template =
     .release {
         font-weight: 700;
     }
+    
     .img {
-        height: 20vw;
-    }
-
-    img {
+        width: 20vw;
         height: 100%;
+    }
+    img {
+        width: 20vw;
+    }
+    .homepage, .imdb {
+        margin-top: 2rem;
     }
 
     @media (max-width: 1500px) {
@@ -56,9 +74,10 @@ const template =
     .wrapper {
         display: block;
         max-width: 740px;
-        width: 74vw;
-        height: 135vw;
-        overflow-y: auto;
+        width: 78vw;
+        height: auto;
+        max-height: 300vw;
+        overflow-y: scroll;
     }
     .img {
         // position: relative;
@@ -71,8 +90,10 @@ const template =
         width: 100%;
     }
     .content {
-        // position: relative;
-        // top: -20vw;
+        display: none;
+        max-height: 100vw;
+        width: 80vw;
+        padding: 0;
     }
 }
 </style>
@@ -82,6 +103,8 @@ const template =
             <h2 class="name"></h2>
             <p class="overview"></p>
             <p class="release"></p>
+            <div class="homepage"></div>
+            <div class="imdb"></div>
         </div>
     </div>
 `;
@@ -94,6 +117,9 @@ export default class Kino extends HTMLElement
         this.name = "Вы шота не ввели";
         this.overview = "Тут могло быть описание, но вы шота не ввели";
         this.release = "Тут могла быть дата, но вы шота не ввели";
+        this.img = "../content/KiT.jpg";
+        this.identificator = 0;
+        this.type = 'hmmm';
     }
 
     connectedCallback()
@@ -105,10 +131,48 @@ export default class Kino extends HTMLElement
             name = this.shadow_root.querySelector('.name'),
             overview = this.shadow_root.querySelector('.overview'),
             release = this.shadow_root.querySelector('.release'),
-            img = this.shadow_root.querySelector('.img');
+            img = this.shadow_root.querySelector('.img'),
+            wrapper = this.shadow_root.querySelector('.wrapper'),
+            content = this.shadow_root.querySelector('.content'),
+            homepage = this.shadow_root.querySelector('.homepage'),
+            imdb = this.shadow_root.querySelector('.imdb');
+        const show = str => {
+            homepage.innerHTML = `<a href="${str.homepage}" target="_blank">Официальная страница</a>`;
+            imdb.innerHTML = `<a href="https://www.imdb.com/title/${str.imdb_id}" target="_blank">Страница IMDB</a>`;
+            homepage.style.height = imdb.style.height = '3rem';
+            console.log(str);
+            if (str.status === 'Ended') release.innerHTML = `
+                <p>${this.release.replace(/-/g, '.')}  -  ${str.last_air_date.replace(/-/g, '.')}</p>
+            `;
+            if (this.type === 'tv') release.innerHTML += `
+                <p style="font-weight: 400">Количество сезонов: ${str.number_of_seasons}</p>
+                <p style="font-weight: 400">Количество эпизодов: ${str.number_of_episodes}</p>
+            `;
+        };
+        const evince = () => {
+            wrapper.removeEventListener('click', evince);
+            console.log(this.identificator + ' ' + this.type);
+            while (this.parentNode.children.length > 1) {
+                if (this.parentNode.firstChild !== this) this.parentNode.removeChild(this.parentNode.firstChild);
+                if (this.parentNode.lastChild !== this) this.parentNode.removeChild(this.parentNode.lastChild);
+            }
+            wrapper.style.border = 'none';
+            wrapper.style.cursor = 'default';
+            wrapper.style.height = 'auto';
+            content.style.display = 'block';
+            content.style.height = 'auto';
+            content.style.paddingTop = '0';
+            console.log(url);
+            fetch(url)
+            .then(response => response.json()) 
+            .then(str => show(str))
+            .catch(reason => console.log);
+            window.scrollTo(0, 0);
+        };
+        const
+            url = `https://api.themoviedb.org/3/${this.type}/${this.identificator}?api_key=6e32fa26fb5f401e54d8a84c649d1e8f&language=ru`;
 
-            if (this.img.includes('null')) this.img = '../content/KiT.jpg';
-
+        if (this.identificator) wrapper.addEventListener('click', evince);
         name.innerHTML = this.name;
         overview.innerHTML = this.overview;
         release.innerHTML = this.release;
@@ -116,11 +180,14 @@ export default class Kino extends HTMLElement
         console.log(this.name, this.overview, this.release);
     }
 
-    setDesc(name, overview, release, img)
+    setDesc(name, overview, release, img, identificator, type)
     {
         this.name = name;
         this.overview = overview;
         this.release = release;
         this.img = img;
+        this.identificator = identificator;
+        this.type = type;
     }
+
 }
--- main.js
+++ main.js
@@ -7,22 +7,35 @@ const init = () => {
     // doc connect
     const
         searchform = document.querySelector('.search-form'),
-        movie = document.querySelector('.movies');
+        movie = document.querySelector('.movies'),
+        refresh = document.querySelector('.refresh');
 
     // functions
     const show = str => {
         const urlPoster = `https://image.tmdb.org/t/p/w500/`;
-        while (movie.firstChild) {
-            movie.removeChild(movie.firstChild);
-        }
         console.log(str.results);
-        str.results.forEach( item => {
+        if (!str.results.length) {
             const kino = document.createElement('view-kino');
+            const
+                poster = '../content/KiT.jpg',
+                name = 'Ничего не нашлось, но можете заценить котика',
+                overview = 'Ну серьёзно, какой крутой кошак',
+                date = 'Такой милаха';
+            kino.setDesc(name, overview, date, poster);
+            movie.appendChild(kino);
+        }
+        str.results.forEach( item => {
+            if (item.media_type === 'person') return; // erase all humans
+            const kino = document.createElement('view-kino');
+            const poster = item.poster_path ? urlPoster + item.poster_path : '../content/KiT.jpg';
+            const type = item.name ? 'tv' : 'movie';
             kino.setDesc(
                 item.name || item.title,
                 item.overview,
                 item.first_air_date || item.release_date,
-                urlPoster + item.poster_path,
+                poster,
+                item.id,
+                type
                 );
             movie.appendChild(kino);
         });
@@ -30,28 +43,56 @@ const init = () => {
     const apiSearch = searchText => {
         const url = `https://api.themoviedb.org/3/search/multi?api_key=6e32fa26fb5f401e54d8a84c649d1e8f&language=ru&query=${searchText}`;
 
+        while (movie.firstChild) {
+            movie.removeChild(movie.firstChild);
+        }
+        const loading = document.createElement('div');
+        loading.className = 'loading';
+        movie.appendChild(loading);
+
+        fetch(url)
+            .then(response => response.json())
+            .then(str => {
+                while (movie.firstChild) { 
+                    movie.removeChild(movie.firstChild);
+                }
+                show(str);
+            })
+            .catch(reason => console.log);
+    };
+    const showPopularOfWeek = () => {
+        const url = 'https://api.themoviedb.org/3/trending/all/week?api_key=6e32fa26fb5f401e54d8a84c649d1e8f&language=ru';
+        while (movie.firstChild) movie.removeChild(movie.firstChild);
+        const popular = document.createElement('h2');
+        popular.innerHTML = 'Популярное за неделю';
+        movie.appendChild(popular);
+
         fetch(url)
             .then(response => response.json())
             .then(str => show(str))
-            .catch(reason => {
-                console.log(reason);
-            });
+            .catch(reason => console.log);
     };
 
     searchform.addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {   
-            let searchText = searchform.value;
-            searchText = searchText.trim();
-            if (!searchText) {
+            if (!searchform.value.trim()) {
                 while (movie.firstChild) {
                     movie.removeChild(movie.firstChild);
                 }
                 const kino = document.createElement('view-kino');
                 movie.appendChild(kino);
             } else {
-                apiSearch(searchText);
+                apiSearch(searchform.value.trim());
             }
         }
     });
+    refresh.addEventListener('click', showPopularOfWeek);
+    showPopularOfWeek();
 }
 document.addEventListener('DOMContentLoaded', init);
+// export const clear = () => {
+//     while (movie.firstChild) {
+//         movie.removeChild(movie.firstChild);
+//     }
+// };
+// export const movies = docunemt.querySelector('.movies');
